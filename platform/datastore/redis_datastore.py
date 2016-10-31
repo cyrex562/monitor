f0df0e8fbbc441d2196db0ce95edac71f89636e7
Josh Madden <cyrex562@gmail.com>
2016-10-30 20:42:01 -0400

    added redis server

---
+++ platform/datastore/redis_datastore.py
@@ -0,0 +1,8 @@
+﻿def run():
+    pass
+
+if __name__ == "__main__":
+    run()
+
+
+# END OF FILE #
---
+++ platform/datastore/vedis_datastore.py
@@ -0,0 +1,9 @@
+from vedis import vedis
+
+def run():
+    db = vedis(':mem:')
+
+if __name__ == "__main__":
+    run()
+
+
